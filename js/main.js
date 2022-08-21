a0de95c0e2ea753326157c5094184a5c6e881302
Yashnarbek <ymqayumov@gmail.com>
2022-08-21 18:03:12 +0300

    Add new section

--- js/main.js
+++ js/main.js
@@ -1,7 +1,27 @@
 let elBurger = document.querySelector(".site-header__burger-mnu")
 let elNav = document.querySelector(".site-header__nav")
 
+let modal = document.querySelector(".modal-box")
+let checkOutMobile = document.querySelector(".check-out-mobile")
+let checkOut = document.querySelector(".check-out")
+let create = document.querySelector(".js-create-plane")
+let body = document.querySelector("body")
+
 elBurger.addEventListener("click", function(){
     elNav.classList.toggle("active")
     elBurger.classList.toggle("close-btn")
+})
+
+create.addEventListener("click", function(){
+    modal.classList.add("active")
+    body.classList.add("scroll")
+})
+
+checkOutMobile.addEventListener("click", function(){
+    modal.classList.remove("active")
+    body.classList.remove("scroll")
+})
+checkOut.addEventListener("click", function(){
+    modal.classList.remove("active")
+    body.classList.remove("scroll")
 })
